6d1ca66c21f6c86e869efefde770f8abd85ccc21
clade <pierre.clade@spectro.jussieu.fr>
2011-10-21 14:44:36 +0200

    Setup.py is working correctly

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 #from distutils.core import setup
 from setuptools import setup
 
-      long_description='''\
+long_description='''\
 Overview
 ========
 
@@ -58,10 +58,11 @@ Main changes:
 # So I remove the accent of the author name in this case
 # TODO: find an automatic way of removing accent if version<2.6
 if sys.version_info[:2]>=(2,6): # Unicode accent does not work on earlier version
-    setup(name="PyDAQmx", version='0.1',
+    setup(name="PyTektronixScope", version='0.1',
       author=u'Pierre Cladé', author_email="pierre.clade@spectro.jussieu.fr",
       maintainer=u'Pierre Cladé',
       maintainer_email="pierre.clade@spectro.jussieu.fr",
+      url='https://github.com/clade/PyTektronixScope',
       license='''\
 This software can be used under one of the following two licenses: \
 (1) The BSD license. \
@@ -89,11 +90,11 @@ author.''',
 
 )
 else: # version of python <2.6. Remove the unicode  
-      setup(name="PyDAQmx", version='0.1',
+      setup(name="PyTektronixScope", version='0.1',
       author='Pierre Clade', author_email="pierre.clade@spectro.jussieu.fr",
       maintainer='Pierre Clade',
       maintainer_email="pierre.clade@spectro.jussieu.fr",
-      url='http://packages.python.org/PyDAQmx/',
+      url='https://github.com/clade/PyTektronixScope',
       license='''\
 This software can be used under one of the following two licenses: \
 (1) The BSD license. \
